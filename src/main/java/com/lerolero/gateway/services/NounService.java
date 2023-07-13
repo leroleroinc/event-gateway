683a467de547f7bebf30d8b1907241f588ccfae9
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2023-07-12 23:18:00 -0300

    cleaning up

--- src/main/java/com/lerolero/gateway/services/NounService.java
+++ src/main/java/com/lerolero/gateway/services/NounService.java
@@ -12,61 +12,28 @@ import org.springframework.messaging.support.MessageBuilder;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
-import reactor.core.publisher.Sinks;
 import java.util.function.Consumer;
 
 @Service
 public class NounService {
 
-//	@Autowired
-//	@Qualifier("nounsWebClient")
-//	private WebClient webClient;
-
 	@Autowired
 	private StreamBridge streamBridge;
 	private StringSink sink = new StringSink();
-	//private Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
 	private Flux<String> stream = Flux.create(sink).share();
 
 	public Flux<String> randomNounList(Integer size) {
-//		return webClient.get()
-//			.uri("/nouns?size=" + size)
-//			.retrieve()
-//			.bodyToFlux(String.class);
 		Message<String> nounCount = MessageBuilder.withPayload(size.toString()).build();
 		streamBridge.send("nounsupplier-out-0", nounCount);
 		System.out.println("GATEWAY: Producing " + size);
-		//return Flux.create(nounSink).take(size);
-//		return sink.asFlux()
-//			.take(size);
-			//.doOnTerminate(() -> sink = Sinks.many().multicast().onBackpressureBuffer());
 		return stream.take(size);
 	}
 
-//	public Flux<String> randomNounEvents(Integer interval) {
-//		return webClient.get()
-//			.uri("/nouns/events?interval=" + interval)
-//			.retrieve()
-//			.bodyToFlux(String.class);
-//	}
-
 	@Bean
 	public Consumer<String> nounconsumer() {
-		for (int i = 0; i < 200; i++) System.out.println("GATEWAY CONSUMER");
 		return noun -> {
 			System.out.println("GATEWAY: Consuming " + noun);
 			sink.produce(noun);
-			//Sinks.EmitResult r = sink.tryEmitNext(noun);
-			//System.out.println(r.isSuccess());
-
-		//	sink.emitNext(noun, new Sinks.EmitFailureHandler() {
-		//		@Override
-		//		public boolean onEmitFailure(reactor.core.publisher.SignalType s, Sinks.EmitResult r) {
-		//			System.out.println(r.isSuccess());
-		//			sink = Sinks.many().multicast().onBackpressureBuffer();
-		//			return false;
-		//		}
-		//	});
 		};
 	}
 
